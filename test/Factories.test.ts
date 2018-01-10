7d9ee439362b362cf98f00e491256a13e269b7df
Jonathan Palumbo <jpalumbo@riskalyze.com>
2018-01-10 13:54:36 -0800

    Fixing broken tests.

--- test/Factories.test.ts
+++ test/Factories.test.ts
@@ -1,17 +1,17 @@
 import { IFileParser } from "../src/interfaces/FileHandlers";
 import JsonParser from "../src/utility/JsonParser";
-import { createParser } from "../src/Factories";
+import { createFileParser } from "../src/Factories";
 
 
-describe("createParser", () => {
+describe("createFileParser", () => {
     it("should return a json parse when the file extension is .json", () => {
-        const parser: IFileParser = createParser("some/path/to/package.json");
+        const parser: IFileParser = createFileParser("some/path/to/package.json");
         expect(parser).toBeInstanceOf(JsonParser);
     });
 
     it("should throw an error for unsupported file extensions ", () => {
         expect(() => {
-            createParser("some/path/to/package.foo");
+            createFileParser("some/path/to/package.foo");
         }).toThrow();
     });
 });
